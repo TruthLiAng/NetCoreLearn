7811ead667b34c6c1e76b368da06e1350d526c4f
zhouliang <zhou@truth>
2019-08-27 15:54:24 +0800

    增加ocelot相关项目及api测试网站

---
+++ OceletGateWay/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OceletGateWay.Models;
+using Ocelot.Logging;
+
+namespace OceletGateWay.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IOcelotLoggerFactory Logger { get; set; }
+
+        public IOcelotLogger log;
+
+        public HomeController(IOcelotLoggerFactory logger)
+        {
+            Logger = logger;
+            log = Logger.CreateLogger<string>();
+        }
+
+        public IActionResult Index()
+        {
+            log.LogInformation("*********Start*************");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ OceletGateWay/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Ocelot.Middleware;
+
+namespace OceletGateWay
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args)
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Startup>()
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.AddJsonFile("ocelot.json").AddEnvironmentVariables();
+                })
+                .Build();
+        }
+    }
+}
